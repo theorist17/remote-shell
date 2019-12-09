b68ec32a31784c40615da9124d1e28916559a554
theorist17 <theorist17@gmail.com>
2019-12-10 02:06:57 +0900

    perform stdout redirecting to socket

--- shell.cpp
+++ shell.cpp
@@ -1,4 +1,10 @@
 #include "shell.hpp"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <iostream>
+
+using namespace std;
 using namespace RemoteShell;
 
 Shell::Shell()
@@ -24,8 +30,32 @@ int Shell::run(){
         valread = read( m_sockfd, m_incoming, 1024); 
         m_incoming[valread] = '\0'; // marking last message NULL 
 
-        // 여기에 쉘의 명령이 들어갑니다.
-        strcpy(m_outgoing, m_incoming);
+        memset(m_outgoing, 0, 1024);
+        if (strcmp(m_incoming, "exit")==0)
+        {
+            strcpy(m_outgoing, m_incoming);
+        }
+        else
+        {
+            char buffer[1024];
+            std::string result = "";
+            FILE* pipe = popen("ls -la .", "r");
+            if (!pipe) throw std::runtime_error("popen() failed!");
+            try {
+                result += " ";
+                while (!feof(pipe)) {
+                    if (fgets(buffer, 1024, pipe) != NULL)
+                        result += buffer;
+                }
+            } 
+            catch (...) {
+                pclose(pipe);
+                result = "Not Found";
+                throw;
+            }
+            pclose(pipe);
+            strcpy(m_outgoing, result.c_str());
+        }
 
         send(m_sockfd, m_outgoing, strlen(m_outgoing) , 0 ); 
         if (strcmp(m_incoming, "exit")==0)
